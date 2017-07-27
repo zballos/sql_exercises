3e26cfd197f162ca9ad9ad906e0536540b50d698
Edson Zeballos <09edson@gmail.com>
2017-07-27 17:57:42 -0400

    Functions: EXTRACT, SUM AND AVG

--- Oracle12c/exercise_4.sql
+++ Oracle12c/exercise_4.sql
@@ -9,3 +9,19 @@ SELECT AVG(value) FROM finances;
 
 # 3. Count tha amount of finances in 2017
 SELECT COUNT(value) FROM finances WHERE payment_date BETWEEN '01-JAN-2017' AND '31-DEZ-2017';
+
+# 4. Calcule the total and average value of all finances
+SELECT SUM(value) AS TOTAL, AVG(value) AS AVERAGE FROM finances;
+
+# 5. Calcule and show the total values by received
+SELECT received, SUM(value) as TOTAL FROM finances GROUP BY received;
+
+# NOTE: The function EXTRACT serves for to extract one element in one value, for example, type date.
+# 6. Sum the total of values by year
+SELECT EXTRACT(YEAR FROM payment_date), SUM(value) FROM finances GROUP BY EXTRACT(YEAR FROM payment_date);
+
+# 7. Now, improve the before exercise and order by year
+SELECT EXTRACT(YEAR FROM payment_date) AS PAYMENT_YEAR, SUM(value) AS TOTAL
+FROM finances
+GROUP BY EXTRACT(YEAR FROM payment_date)
+ORDER BY PAYMENT_YEAR;
