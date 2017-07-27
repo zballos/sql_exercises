fbbca1045592d64bced502dd00db58e36b6b7d7a
Edson Zeballos <09edson@gmail.com>
2017-07-27 18:25:36 -0400

    Finish exercise 4

--- Oracle12c/exercise_4.sql
+++ Oracle12c/exercise_4.sql
@@ -7,21 +7,37 @@ SELECT SUM(value) FROM finances;
 # 2. Return the average of all values in finances
 SELECT AVG(value) FROM finances;
 
-# 3. Count tha amount of finances in 2017
+# 3. Count the amount of finances in 2017
 SELECT COUNT(value) FROM finances WHERE payment_date BETWEEN '01-JAN-2017' AND '31-DEZ-2017';
 
-# 4. Calcule the total and average value of all finances
+# 4. Calculate the total and average value of all finances
 SELECT SUM(value) AS TOTAL, AVG(value) AS AVERAGE FROM finances;
 
-# 5. Calcule and show the total values by received
+# 5. Calculate and show the total values by received
 SELECT received, SUM(value) as TOTAL FROM finances GROUP BY received;
 
-# NOTE: The function EXTRACT serves for to extract one element in one value, for example, type date.
+# NOTE: The EXTRACT function is for extracting an element from a date, into date type columns
 # 6. Sum the total of values by year
 SELECT EXTRACT(YEAR FROM payment_date), SUM(value) FROM finances GROUP BY EXTRACT(YEAR FROM payment_date);
 
 # 7. Now, improve the before exercise and order by year
 SELECT EXTRACT(YEAR FROM payment_date) AS PAYMENT_YEAR, SUM(value) AS TOTAL
-FROM finances
-GROUP BY EXTRACT(YEAR FROM payment_date)
-ORDER BY PAYMENT_YEAR;
+	FROM finances
+	GROUP BY EXTRACT(YEAR FROM payment_date)
+	ORDER BY PAYMENT_YEAR;
+
+# REVISION
+# 8. Calculate the average of all finances, the date is less of 12/04/2017
+SELECT AVG(value) AS AVERAGE FROM finances WHERE payment_date <= '12-ABR-2017';
+
+# 9. Sum all values before 10/07/2016 and group by payment type
+SELECT payment_type, SUM(value) AS TOTAL 
+	FROM finances 
+	WHERE payment_date <= '10-JUL-2016' 
+	GROUP BY payment_type; 
+
+# 10. Calculate the total of finances received with date less 12/01/2017
+SELECT COUNT(id) AS TOTAL FROM finances WHERE payment_date < '12-JAN-2017' AND received = '1';
+
+# 11. Show total of all values group by received and payment type
+SELECT received, payment_type, SUM(value) AS TOTAL FROM finances GROUP BY received, payment_type;
