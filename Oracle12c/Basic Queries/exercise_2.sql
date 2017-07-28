67eaff7617a7077010ae667cb8653e45e624ced7
Edson Zeballos <09edson@gmail.com>
2017-07-28 11:55:26 -0400

    Correct tag comment for oracle database

--- Oracle12c/Basic Queries/exercise_2.sql
+++ Oracle12c/Basic Queries/exercise_2.sql
@@ -1,40 +1,40 @@
-# Exercise 2: I continue using the last table 'finances'.
+-- Exercise 2: I continue using the last table 'finances'.
 
-# 1. Select values in specific interval of values
+-- 1. Select values in specific interval of values
 SELECT * FROM finances WHERE value >= 500 AND value <= 1500;
 
-# 2. Repeat using between
+-- 2. Repeat using between
 SELECT * FROM finances WHERE value BETWEEN 500 AND 1500;
 
-# 3. Filter using between, but with interval of dates
+-- 3. Filter using between, but with interval of dates
 SELECT * FROM finances WHERE my_date BETWEEN '10-JAN-2017' AND '10-JUN-2017';
 
-# 4. Repeat last exercise, ordering the result for date
+-- 4. Repeat last exercise, ordering the result for date
 SELECT * FROM finances WHERE my_date BETWEEN '10-JAN-2017' AND '10-JUN-2017' ORDER BY my_date ASC;
 
-# 5. Update all observations when date < JAN/2016
+-- 5. Update all observations when date < JAN/2016
 UPDATE finances SET observation = 'OLD COUNTS, IGNORE!' WHERE my_date < '01-JAN-2016';
 
-# 6. Now, update all observations that not between JUN/2015 until OCT/2017
+-- 6. Now, update all observations that not between JUN/2015 until OCT/2017
 UPDATE finances SET observation = 'specific values' WHERE my_date NOT BETWEEN '01-JUN-2015' AND '01-OUT-2017';
 
-# 7. Update finances in specific dates
+-- 7. Update finances in specific dates
 UPDATE finances SET observation = 'in holidays' WHERE my_date IN ('13-JUN-2017', '07-SET-2016', '31-DEZ-2015'); 
 
-# 8. Delete one register with date equal 02-FEV-2015
+-- 8. Delete one register with date equal 02-FEV-2015
 DELETE FROM finances WHERE my_date < '20-FEV-2015';
 
-# 9. Update purchases when date equals 12/06/2017;
+-- 9. Update purchases when date equals 12/06/2017;
 UPDATE finances set observation = 'purchases in VALENTINE´s DAY' WHERE my_date = '12-JUN-2017';
 
-# 10. Update all finances, sum actual value + 10 only when date is less that 01/01/2016
+-- 10. Update all finances, sum actual value + 10 only when date is less that 01/01/2016
 UPDATE finances SET value = value + 10 WHERE my_date < '01-JAN-2016'; 
 
-# 11. Update all finances that date between 01/07/2015 and 01/07/2016, with observation and received true.
+-- 11. Update all finances that date between 01/07/2015 and 01/07/2016, with observation and received true.
 UPDATE finances SET observation = 'payed before 2017', received = '1' WHERE my_date BETWEEN '01-SET-2015' AND '01-SET-2016';
 
-# 12. Delete all finances that date between 02/03/2016 until 20/03/2016
+-- 12. Delete all finances that date between 02/03/2016 until 20/03/2016
 DELETE FROM finances WHERE my_date BETWEEN '02-MAR-2016' AND '20-MAR-2016';
 
-# 13. Show all finances that value not equals 98,00
+-- 13. Show all finances that value not equals 98,00
 SELECT * FROM finances WHERE NOT value = 98; 
--- Oracle12c/Basic Queries/exercise_5.sql
+++ Oracle12c/Basic Queries/exercise_5.sql
@@ -1,6 +1,6 @@
-# Exercise 5: News features, new table, relationships.
+-- Exercise 5: News features, new table, relationships.
 
-# 1. Create new table for buyers
+-- 1. Create new table for buyers
 CREATE TABLE buyers (
 	id NUMBER PRIMARY KEY,
 	name VARCHAR2(40) NOT NULL,
@@ -8,52 +8,52 @@ CREATE TABLE buyers (
 	telephone VARCHAR2(20) NOT NULL
 );
 
-# 2. Create the sequence for table buyers
+-- 2. Create the sequence for table buyers
 CREATE SEQUENCE id_buyers_seq;
 
-# 3. Insert three buyers 
+-- 3. Insert three buyers 
 INSERT INTO buyers (id, name, address, telephone) VALUES (id_buyers_seq.nextval, 'Albert', 'batcavern', '(55) 8181-9901');
 INSERT INTO buyers (id, name, address, telephone) VALUES (id_buyers_seq.nextval, 'Bruce Wayne', 'batcavern', '(55) 8181-9901');
 INSERT INTO buyers (id, name, address, telephone) VALUES (id_buyers_seq.nextval, 'Thomas Wayne', 'batcavern', '(55) 8181-9901');
 
-# NOTE: In prompt command, thare are a crash in the table buyers because the size is minimal. If you want change, use the command:
+-- NOTE: In prompt command, thare are a crash in the table buyers because the size is minimal. If you want change, use the command:
 SET LINESIZE 150; 
 
-# Well, now we need connect the table finances with the table buyers. Ready go!
-# 4. Create a new column in table finances, with name 'buyer_id'
+-- Well, now we need connect the table finances with the table buyers. Ready go!
+-- 4. Create a new column in table finances, with name 'buyer_id'
 ALTER TABLE finances ADD (buyer_id NUMBER); 
 
-# 5. Set all finances with id less than 140 for the buyer 'Albert'
+-- 5. Set all finances with id less than 140 for the buyer 'Albert'
 UPDATE finances SET buyer_id = 1 WHERE id < 150;
 
-# 6. Set all finances with id greater than or equals 150 for buyer 'Bruce Wayne'
+-- 6. Set all finances with id greater than or equals 150 for buyer 'Bruce Wayne'
 UPDATE finances SET buyer_id = 2 WHERE id >= 150;
 
-# NOTE: The JOIN command is used to join two tables with a given ID in each table.
-# 7. Select observation, value, payment_date and name in table finances and buyers
+-- NOTE: The JOIN command is used to join two tables with a given ID in each table.
+-- 7. Select observation, value, payment_date and name in table finances and buyers
 SELECT observation, value, payment_date, name
 FROM finances 
 JOIN buyers ON (buyers.id = finances.buyer_id);
 
-# 8. Configure a foreign key in table finances
+-- 8. Configure a foreign key in table finances
 ALTER TABLE finances ADD FOREIGN KEY (buyer_id) REFERENCES buyers(id);
 
-# REVISION
-# 9. Shows name and value of finances before 08/03/2017
+-- REVISION
+-- 9. Shows name and value of finances before 08/03/2017
 SELECT name, value 
 FROM finances JOIN buyers ON buyers.id = finances.buyer_id
 WHERE payment_date <= '08-ABR-2017';
 
-# 10. Show all finances that buyer id is 1
+-- 10. Show all finances that buyer id is 1
 SELECT * FROM finances WHERE buyer_id = 1;
 SELECT * FROM finances JOIN buyers ON buyers.id = finances.buyer_id WHERE buyers.id = 1;
 
-# 11. Show finances that contains name 'Wayne' in buyers
+-- 11. Show finances that contains name 'Wayne' in buyers
 SELECT finances.*
 FROM finances JOIN buyers ON buyers.id = finances.buyer_id
 WHERE buyers.name LIKE '%Wayne%';
 
-# 12. Show only name and value total of all buyers
+-- 12. Show only name and value total of all buyers
 SELECT name, SUM(value) as TOTAL
 FROM finances JOIN buyers ON buyers.id = finances.buyer_id
 GROUP BY buyers.name ; 
