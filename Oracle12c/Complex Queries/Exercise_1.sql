495e07d428cee8242b53c9a35d197ad107e4c073
Edson Zeballos <09edson@gmail.com>
2017-07-30 18:32:20 -0400

    Initializing the complex queries

---
+++ Oracle12c/Complex Queries/Exercise_1.sql
@@ -0,0 +1,6 @@
+-- New functions and functionalities of Oracle
+-- Remember how to show the language?
+show parameter nls_lang;
+
+-- If we wanted to show all tables of the logged user?
+select table_name from user_tables;
