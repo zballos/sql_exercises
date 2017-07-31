248759e82b463b1d3207af1c8dd50f75363b8ac1
Edson Zeballos <09edson@gmail.com>
2017-07-31 17:31:46 -0400

    Exercise 2 - Joins, group by and count

---
+++ Oracle12c/ComplexQueries/Exercise_2.sql
@@ -0,0 +1,25 @@
+-- Exercise 2: Joins, group by, count...
+
+-- Before all, remember the structure of tables using DESC:
+DESC employees;
+DESC roles;
+DESC department;
+DESC companies;
+DESC benefits;
+DESC employees_benefits;
+DESC companies_departments;
+
+-- 1. Show name and amount of employees by company
+
+-- 2. Show name and amount of employees by department
+
+-- 3. Show employee, benefit name and total value of benefits by employees in 2017
+
+-- 4. Show company name and total value of benefits after JAN-2015
+
+-- 5. Calculate the average of benefits of all employees
+
+-- 6. Calculate the average of salaries by company
+
+-- 7. Show all employees and ordered by name desc
+ 
