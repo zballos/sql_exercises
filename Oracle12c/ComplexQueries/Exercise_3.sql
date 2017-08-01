f5fb3564e2e056477221f3d23175d1fe2531315b
Edson Zeballos <09edson@gmail.com>
2017-07-31 20:08:24 -0400

    Exercise 3: Having

---
+++ Oracle12c/ComplexQueries/Exercise_3.sql
@@ -0,0 +1,8 @@
+-- Exercise 3: HAVING and agregations
+-- Use HAVING for selects with group by, for example.
+
+-- 1. Show the average of benefits of all employees when average less 258,00.
+
+-- 2. Show the companies that have greater than 10 employees.
+
+-- 3. Show the employee name and total value of benefits received, only when the total is greater than 500,00.
