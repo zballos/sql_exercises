199dbb6a36fedd401841e3947d01d02cf0b8cce5
Edson Zeballos <09edson@gmail.com>
2017-08-06 17:11:27 -0400

    Select using rownum and tests

--- Oracle12c/ComplexQueries/Exercise_7.sql
+++ Oracle12c/ComplexQueries/Exercise_7.sql
@@ -1,1 +1,35 @@
 -- Exercise 7: ROWNUM
+
+-- NOTE: Increase the page size
+set pagesize 50;
+
+-- 1. Show all employees ordered by name and number
+SELECT ROWNUM, name FROM (
+    SELECT e.name FROM employees e ORDER BY e.name
+);
+
+-- 2. Show only the 10 first employees
+SELECT ROWNUM, name
+FROM (
+    SELECT e.name FROM employees e ORDER BY e.name
+)
+WHERE ROWNUM <= 10;
+
+-- 3. If i want only employees between 10 and 20
+SELECT * FROM (
+    SELECT ROWNUM num, name
+    FROM (
+        SELECT e.name FROM employees e ORDER BY e.name
+    )
+    WHERE ROWNUM <= 20
+)
+WHERE num > 10;
+
+-- 4. Now show only employees after rownum 20
+SELECT * FROM (
+    SELECT ROWNUM num, name
+    FROM (
+        SELECT e.name FROM employees e ORDER BY e.name
+    )
+)
+WHERE num > 20;
