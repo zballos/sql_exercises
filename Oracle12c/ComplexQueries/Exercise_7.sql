6201723c886feafeda8eda8b57b7df81eb230b28
Edson Zeballos <09edson@gmail.com>
2017-08-06 17:20:26 -0400

    More conditionals using rownum

--- Oracle12c/ComplexQueries/Exercise_7.sql
+++ Oracle12c/ComplexQueries/Exercise_7.sql
@@ -33,3 +33,30 @@ SELECT * FROM (
     )
 )
 WHERE num > 20;
+
+-- 5. Show all employees that name starts with word 'M'
+SELECT ROWNUM num, name
+FROM (
+    SELECT e.name FROM employees e WHERE e.name LIKE 'M%' ORDER BY e.name
+);
+
+-- 6. Show employees that age between 18 and 30 years old
+SELECT ROWNUM num, name, age
+FROM (
+    SELECT e.name, e.age
+    FROM employees e 
+    WHERE e.age BETWEEN 18 AND 30
+    ORDER BY e.name
+);
+
+-- 7. Show only 10 first employees that age between 18 and 30 years old
+SELECT * FROM (
+    SELECT ROWNUM num, name, age
+    FROM (
+        SELECT e.name, e.age
+        FROM employees e 
+        WHERE e.age BETWEEN 18 AND 30
+        ORDER BY e.name
+    )
+)
+WHERE num <= 10;
