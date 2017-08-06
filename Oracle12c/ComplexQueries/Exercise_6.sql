39d5b8719403895822aadef3166977bd33707245
Edson Zeballos <09edson@gmail.com>
2017-08-06 16:37:02 -0400

    test left and right join

--- Oracle12c/ComplexQueries/Exercise_6.sql
+++ Oracle12c/ComplexQueries/Exercise_6.sql
@@ -6,3 +6,24 @@ FROM employees e
 JOIN employees_benefits eb ON (eb.employee_id = e.id)
 JOIN benefits b ON (eb.benefit_id = b.id)
 GROUP BY e.name, b.name, eb.benefit_value;
+
+-- 2. Insert new employee
+DESC employees;
+
+INSERT INTO employees (id, name, age, role_id, company_id)
+VALUES (id_employees_seq.nextval, 'Jones Robert', 31, 9, 3);
+
+-- 3. Select all employees using LEFT JOIN
+SELECT e.name AS employee, b.name as benefit, eb.benefit_value
+FROM employees e
+LEFT JOIN employees_benefits eb ON (eb.employee_id = e.id)
+LEFT JOIN benefits b ON (eb.benefit_id = b.id)
+GROUP BY e.name, b.name, eb.benefit_value;
+
+-- 4. Show all using RIGHT JOIN from the benefits table
+SELECT e.name AS employee, b.name as benefit, eb.benefit_value
+FROM benefits b
+RIGHT JOIN employees_benefits eb ON (eb.benefit_id = b.id)
+RIGHT JOIN employees e ON (eb.employee_id = e.id)
+GROUP BY e.name, b.name, eb.benefit_value
+ORDER BY e.name ASC;
