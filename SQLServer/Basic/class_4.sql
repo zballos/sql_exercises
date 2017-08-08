ccf44e0588b506ed1c44403f3c833db79dda3f0d
Edson Zeballos <09edson@gmail.com>
2017-08-07 23:10:03 -0400

    avg, count, month and year

--- SQLServer/Basic/class_4.sql
+++ SQLServer/Basic/class_4.sql
@@ -12,4 +12,15 @@ FROM purchases
 WHERE purchase_date <= '2009-09-10'
 GROUP BY received; 
 
--- 4. 
+-- 4. Count total of purchases, group by received
+SELECT received, COUNT(1) AS amount
+FROM purchases
+GROUP BY received;
+
+-- 5. Count total amount and value of purchases, and group to received, month and year
+SELECT YEAR(purchase_date) AS year, MONTH(purchase_date) AS month, received, COUNT(1) AS amount, SUM(value) as total
+FROM purchases
+GROUP BY YEAR(purchase_date), MONTH(purchase_date), received;
+
+-- 6. Show average all purchases
+SELECT AVG(value) AS average FROM purchases;
