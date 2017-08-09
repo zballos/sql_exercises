a93c1848e3d09fecde8c2c0bfcf5e677665b3b4f
Edson Zeballos <09edson@gmail.com>
2017-08-08 21:53:16 -0400

    foreign key buyer_id in purchases

--- SQLServer/Basic/class_5.sql
+++ SQLServer/Basic/class_5.sql
@@ -10,3 +10,19 @@ CREATE TABLE buyers(
 
 -- 2. Insert column for id buyers associated with purchases
 ALTER TABLE purchases ADD buyer_id INT NULL;
+
+-- 3. Create new buyers
+INSERT INTO buyers VALUES ('João da Silva', 'Rua 123', '');
+INSERT INTO buyers VALUES ('Marc Zuk', 'Facebook Street', '');
+INSERT INTO buyers VALUES ('Dave Jones', 'Don Juan Avenue', '123456789');
+
+-- 4. Distribute purchases between buyers
+UPDATE purchases SET buyer_id = 1 WHERE id < 25;
+UPDATE purchases SET buyer_id = 2 WHERE id > 25;
+UPDATE purchases SET buyer_id = 3 WHERE id = 25;
+
+-- 5. Add foreign key
+ALTER TABLE purchases 
+ADD CONSTRAINT FK_Purchases_Buyers_ID 
+FOREIGN KEY (buyer_id)
+REFERENCES buyers (id);
