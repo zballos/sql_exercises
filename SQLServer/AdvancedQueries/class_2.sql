820af524a03c8699b0dd64995c8feb3aee2f149c
Edson Zeballos <09edson@gmail.com>
2017-08-09 22:18:18 -0400

    AVG(), WHERE and JOINs

---
+++ SQLServer/AdvancedQueries/class_2.sql
@@ -0,0 +1,12 @@
+-- WHERE, GROUP BY, GROUP BY with WHERE and COUNT()
+
+-- 1. Show all tables (courses, students, registrations, sections, notes, exercises and answers)
+SELECT * FROM course;
+SELECT * FROM student;
+SELECT * FROM registration;
+SELECT * FROM section;
+SELECT * FROM note;
+SELECT * FROM exercise;
+SELECT * FROM answer;
+
+
